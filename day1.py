ac3505634028ca9b9bbfc97a953f473e315135be
Marwan-Alzain-AI-journey <customer.mrwan11@gmail.com>
2026-06-26 23:39:32 +0300

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,100 @@
+character_name = "marwan"
+character_age = "24"
+is_male = True 
+is_female = False
+print('hello world')
+print("I'm good")
+print("    /|")
+print("   / |")
+print("  /  |")
+print(" /___|")
+print("there once there is a man named marwan")
+print("he was 24 years old")
+print("he liked his name marwan")
+print("but he did not like being 24")
+character_age = "35"
+character_name = "ali"
+print("there once there is a man named " + character_name + " ' ")
+print("he was " + character_age + " years old")
+character_name = "ahmad"
+character_age = "50"
+print("he liked his name " +character_name)
+print("but he did not like being " + character_age + "." )
+print("ahmad\nalnasiry")
+print("ahmad\"alnasiry")
+phrase = "mohammad"
+print(phrase + " is handsome")
+print(phrase.upper())
+print(phrase.isupper())
+print(phrase.upper() .isupper())
+print(phrase.capitalize())
+print(len(phrase))
+print(phrase[0])
+print(phrase[0:4])
+print(phrase.index("amm"))
+print(phrase.replace("mohammad" , "silo"))
+print(2)
+print(-2.2343)
+print(-2+5.5)
+print(9/3)
+print(9*9)
+print(phrase)
+print(5+5*5)
+print((5+5)*5)
+print(100%3)
+print(99%3)
+print(98%3)
+my_nums2 = 2
+my_nums = 66
+print(my_nums)
+print(str(my_nums) + (" my favorate mumber"))
+print(abs(my_nums2))
+print(str(my_nums) + str(my_nums2))
+result = my_nums2 + my_nums
+print(result)
+print(pow(3,2))
+print(pow(my_nums, my_nums2))
+print(max(55,3))
+print(min(55,3))
+average = (my_nums + my_nums2 ) / 2
+print(average)
+from math import *
+print(round(3.6))
+print(round(5.4))
+print(floor(5.9))
+print(ceil(5.1))
+print(sqrt(9))
+
+
+ch_name = "marwan"
+ch_age = 24
+frs_num = 50
+sec_num = 10
+trd_num =3
+first_result = frs_num + sec_num + trd_num
+second_result = frs_num - sec_num - trd_num
+third_result = frs_num * sec_num * trd_num
+print(first_result)
+print(second_result)
+print(third_result)
+print(third_result / second_result)
+print(third_result // second_result)
+print("my name is "+ ch_name.capitalize())
+print("I am " + str (ch_age) + " years old ")
+print(ch_name.upper())
+print(ch_name.upper() .islower())
+print(" he wes\nloking for\" a job")
+print(len(ch_name))
+print(ch_name[0:6])
+print(ch_name.index("n"))
+print(ch_name.replace("marwan" , "mother"))
+print((third_result + second_result) - first_result )
+print(third_result%first_result)
+print(abs(second_result - third_result))
+print(pow(frs_num, trd_num))
+print(max(first_result, second_result))
+print(min(first_result, second_result))
+print(floor(third_result/second_result))
+print(ceil(third_result/second_result))
+print(round(third_result/second_result))
+print(sqrt(third_result))
